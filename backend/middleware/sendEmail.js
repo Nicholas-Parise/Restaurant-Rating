0003949128eb92af7bed7b762e817ff6ec7f81d7
Nicholas Parise <1treasurehound@gmail.com>
2026-03-05 00:58:51 -0500

    name updated

--- backend/middleware/sendEmail.js
+++ backend/middleware/sendEmail.js
@@ -12,7 +12,7 @@ const mg = mailgun.client({
 const sendEmail = async (to, subject, text, html) => {
 
   const data = {
-    from: "TBD Support Team <support@mail.TBD.ca>",
+    from: "RestauRate Support Team <support@mail.RestauRate.com>",
     to,
     subject,
     html: html || text,
