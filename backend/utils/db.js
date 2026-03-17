2be8ff10846744d9933f5208c634aa979d1a1fc4
Nicholas Parise <1treasurehound@gmail.com>
2026-03-17 14:21:58 -0400

    updated env

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -7,18 +7,20 @@ console.log("Database port:", process.env.DBPORT);
 console.log("Database name:", process.env.DBNAME);
 
 const pool = new Pool({
+  /*
   user: process.env.DBUSERNAME,
   password: process.env.DBPASSWORD,
   host: process.env.PROD_HOST,
   port: process.env.DBPORT, // default Postgres port 5432
   database: process.env.DBNAME
-  // connectionString: process.env.DATABASE_URL
+  */
+  connectionString: process.env.DATABASE_URL
 });
 
 async function connectWithRetry() {
   while (true) {
     try {
-      await pool.query("SELECT 1");
+      await pool.query("SELECT 1;");
       console.log("Connected to database");
       break;
     } catch (err) {
