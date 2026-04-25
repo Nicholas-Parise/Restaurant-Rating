9a6d9838658aaf00a91ecb084b6a6585afca2101
Nicholas Parise <1treasurehound@gmail.com>
2026-04-24 23:25:19 -0400

    ssr bug fixes

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -5,7 +5,11 @@ export const serverRoutes: ServerRoute[] = [
   { path: 'privacy-policy', renderMode: RenderMode.Prerender },
   { path: 'terms-of-service', renderMode: RenderMode.Prerender },
   {
-    path: '**',
+    path: 'restaurant/:slug',
     renderMode: RenderMode.Server
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Client
   }
 ];
