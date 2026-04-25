98e184f5b74755a88be55e78b7eca57b11c70e34
Nicholas Parise <1treasurehound@gmail.com>
2026-04-24 23:36:02 -0400

    map fix

--- frontend/src/app/restaurant-map/restaurant-map.component.ts
+++ frontend/src/app/restaurant-map/restaurant-map.component.ts
@@ -1,11 +1,11 @@
-import { AfterViewInit, Component, Input } from '@angular/core'
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core'
 import { isPlatformBrowser } from '@angular/common';
 import { inject, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-restaurant-map',
   imports: [],
-  template: '<div id="map" style="height:400px;width:100%"></div>',
+  template: '<div #mapContainer style="height:400px;width:100%"></div>',
   styleUrl: './restaurant-map.component.css',
   standalone: true
 })
@@ -17,13 +17,15 @@ export class RestaurantMapComponent implements AfterViewInit{
   @Input() lon!: number
   @Input() name!: string
 
+  @ViewChild('mapContainer', { static: false }) mapContainer!: ElementRef;
+
 
   async ngAfterViewInit() {
     if (!isPlatformBrowser(this.platformId)) return;
 
     const L = await import('leaflet');
 
-    const map = L.map('map').setView([this.lat, this.lon], 16)
+    const map = L.map(this.mapContainer.nativeElement).setView([this.lat, this.lon], 16)
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap contributors'
